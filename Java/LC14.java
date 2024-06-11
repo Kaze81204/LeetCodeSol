e4cffc032154c14112f9b81d1c52e0af82e551e5
KillerQueen4 <127944663+KillerQueen4@users.noreply.github.com>
2024-06-11 22:48:44 +0530

    Added the concept

---
+++ Java/LC14.java
@@ -0,0 +1,36 @@
+/*
+14. Longest Common Prefix
+Write a function to find the longest common 
+prefix string amongst an array of strings.
+
+If there is no common prefix, return an 
+empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Concept: After sorting the array, by comparing the first and last
+strings and getting the common prefix of both will give
+us the longest common prefix in the array, no need 
+to compare with the strings in between the first and last
+one.
+
+*/
+
+
+
+package Java;
+public class LC14 {
+    public String longestCommonPrefix(String[] strs){
+        
+
+
+
+        return null;
+        
+    }
+}
