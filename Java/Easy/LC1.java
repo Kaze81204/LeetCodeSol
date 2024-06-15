b12b59e368802a3333d0b1b90bc1155b6af31066
KillerQueen4 <127944663+KillerQueen4@users.noreply.github.com>
2024-06-15 17:22:25 +0530

    Text edited

--- Java/Easy/LC1.java
+++ Java/Easy/LC1.java
@@ -2,18 +2,23 @@ package Java.Easy;
 
 
 /* Two Sum
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice.
 
 You can return the answer in any order.
- * 
- * Input: nums = [2,7,11,15], target = 9
+ 
+Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-
-
+Solved using HashMap:
+    Using HashMap to save each element if its complement is not
+    present in the HashMap. 
+    Time Complexity: O(n)
+    Space Complexity: O(n)
  */
 
 
